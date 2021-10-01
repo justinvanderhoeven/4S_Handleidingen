c2eb4a524edb7cf742b3a3484ea7d598b96fd656
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2021-10-01 12:30:07 +0200

    #9
    link om de manual te downloaden is een button geworden met gepaste styling

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -19,10 +19,10 @@
 	@foreach ($manuals as $manual)
 	
 		@if ($manual->locally_available)
-			<a href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/{{ $type->id }}/{{ $type->name_url_encoded }}/{{ $manual->id }}/" alt="{{ __('misc.view_manual_alt') }}" title="{{ __('misc.view_manual_alt') }}">{{ __('misc.view_manual') }}</a> 
+		<button class="manual-button"><a href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/{{ $type->id }}/{{ $type->name_url_encoded }}/{{ $manual->id }}/" alt="{{ __('misc.view_manual_alt') }}" title="{{ __('misc.view_manual_alt') }}">{{ __('misc.view_manual') }}</a></button> 
 			({{$manual->filesize_human_readable}})
 		@else
-			<a href="{{ $manual->url }}" target="new" alt="{{ __('misc.download_manual_alt') }}" title="{{ __('misc.download_manual_alt') }}">{{ __('misc.download_manual') }}</a>
+		<button class="manual-button"><a href="{{ $manual->url }}" target="new" alt="{{ __('misc.download_manual_alt') }}" title="{{ __('misc.download_manual_alt') }}">{{ __('misc.download_manual') }}</a></button>
 		@endif
 		
 		<br />
--- resources/views/pages/manual_view.blade.php
+++ resources/views/pages/manual_view.blade.php
@@ -17,10 +17,10 @@
 @if ($manual->locally_available)
 	<iframe src="{{ $manual->url }}" width="780" height="600" frameborder="0" marginheight="0" marginwidth="0">
 	Iframes are not supported<br />
-	<a href="{{ $manual->url }}" target="new" alt="Download your manual here" title="Download your manual here">Click here to download the manual</a>
+		<a href="{{ $manual->url }}" target="new" alt="Download your manual here" title="Download your manual here">Click here to download the manual</a>	
 	</iframe>
 @else
-	<a href="{{ $manual->url }}" target="new" alt="Download your manual here" title="Download your manual here">Click here to download the manual</a>
+		<a href="{{ $manual->url }}" target="new" alt="Download your manual here" title="Download your manual here">Click here to download the manual</a>	
 @endif
 
 
